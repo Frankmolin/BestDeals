1dbd4e83fdc47c0120ca9b49004417f44ddbf94f
frank <frankmolinaj9604@hotmail.com>
2022-07-30 16:30:06 -0300

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,31 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Deals Games</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className="offcanvas offcanvas-end text-bg-dark" tabIndex={-1} id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
-                    <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasNavbar2Label">Menu</h5>
-                        <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" />
-                    </div>
-                    <div className="offcanvas-body">
-                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/stores">Stores</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
-                            </li>
-                        </ul>
+        <div className="container">
+            <header className="d-flex justify-content-center py-3">
+                <ul className="nav nav-pills">
+                    <li className="nav-item">
+                        <Link to='/' className="nav-link active" aria-current="page">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/stores' className="nav-link">
+                            Stores
+                        </Link>
+                    </li>
+                </ul>
+            </header>
+        </div>
 
-                    </div>
-                </div>
-            </div>
-        </nav>
 
 
     )
